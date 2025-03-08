735bc5dd0f953b0ec72943fe51b01dba90cb18d5
akhdanrgya <akhdan.anargya@gmail.com>
2025-03-08 16:14:35 +0700

    Coloring purple

--- components/MainPage/Hero.tsx
+++ components/MainPage/Hero.tsx
@@ -4,11 +4,11 @@ const Hero = () => {
             <div className="flex flex-col items-start gap-4">
                 <div className="text-4xl">
                     <h1>Hello</h1>
-                    <h1>We Are <span className="text-purple-500">Gokiltech!</span></h1>
+                    <h1>We Are <span className="text-purple">Gokiltech!</span></h1>
                     <h1>Where Tech Meets Creativity</h1>
                 </div>
                 <div className="text-xs">
-                    <p>community of developers and <span className="text-purple-500">tech</span> enthusiasts exploring,
+                    <p>community of developers and <span className="text-purple">tech</span> enthusiasts exploring,
                         innovating, and growing together in the world of technology.</p>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,8 +13,13 @@ module.exports = {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-mono)"],
       },
+      colors: {
+        purple: {
+          DEFAULT: "#9F06A7",
+        },
+      },
     },
   },
   darkMode: "class",
   plugins: [heroui()],
-}
+};
