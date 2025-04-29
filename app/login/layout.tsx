dc36379bb91a66175f4816c765e4b6e990a258b3
akhdanrgya <akhdan.anargya@gmail.com>
2025-04-29 22:59:42 +0700

    Benerin layout login

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,11 +1,16 @@
+import {Navbar} from "@/components/navbar";
+
 export default function DashboardLayout({
                                             children,
                                         }: {
     children: React.ReactNode;
 }) {
     return (
-        <div className="pt-16">
-            {children}
+        <div>
+            <Navbar/>
+            <div className="pt-16">
+                {children}
+            </div>
         </div>
     );
 }
