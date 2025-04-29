78cbdc612b4de82c7ffa09cc2a6df939173e7060
akhdanrgya <akhdan.anargya@gmail.com>
2025-04-29 22:49:38 +0700

    update main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Hero from "@/components/MainPage/Hero"
 import Skills from "@/components/MainPage/Skills";
+import {Navbar} from "@/components/navbar";
 
 
 const Home = () => {
     return (
         <>
+            <Navbar/>
             <Hero/>
             <Skills/>
+            <footer className="w-full flex items-center justify-center py-3">
+                <p className="text-primary">Gokiltech</p>
+            </footer>
         </>
     )
 }
