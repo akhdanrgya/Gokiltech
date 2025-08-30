ee3699efdba99a318d697be72f4ac616d80fba18
akhdanrgya <akhdan.anargya@gmail.com>
2025-08-30 18:25:25 +0700

    otw profile card team

--- components/MainPage/Team.tsx
+++ components/MainPage/Team.tsx
@@ -1,20 +1,7 @@
-import ProfileCard from "@/src/blocks/Components/ProfileCard/ProfileCard";
-
 const Team = () => {
   return (
-    <section>
-      <ProfileCard
-        name="Akhdan Anargya Arisadi"
-        title="Software Engineer"
-        handle="akhdanrgya"
-        status="Online"
-        contactText="Contact Me"
-        avatarUrl="images/icon.png"
-        showUserInfo={true}
-        enableTilt={true}
-        enableMobileTilt={false}
-        onContactClick={() => console.log("Contact clicked")}
-      />
+    <section className="">
+      <div className="flex gap-4 justify-center items-center"></div>
     </section>
   );
 };
