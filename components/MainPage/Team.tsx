3d90b5c0776c7d22ab9a060e1cad8eeb146cbd58
akhdanrgya <akhdan.anargya@gmail.com>
2025-08-30 18:38:30 +0700

    update

--- components/MainPage/Team.tsx
+++ components/MainPage/Team.tsx
@@ -1,7 +1,13 @@
+import React from "react";
+
 const Team = () => {
   return (
-    <section className="">
-      <div className="flex gap-4 justify-center items-center"></div>
+    <section className="px-6 flex items-center min-h-screen">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+        <p className="text-sm font-semibold text-purple uppercase mb-2">
+          OUR TEAMS
+        </p>
+      </div>
     </section>
   );
 };
